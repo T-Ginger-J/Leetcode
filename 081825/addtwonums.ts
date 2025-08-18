b144282d83f6e715724bd4e0265c4bdbdbe57177
Trent <Trent.Jordan@ontariotechu.net>
2025-08-18 11:03:11 -0400

    addtwonums recursive
    redoing with recursion

--- 081825/addtwonums.ts
+++ 081825/addtwonums.ts
@@ -29,3 +29,26 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
 
     return dummy.next;
 };
+
+function addTwoNumbersRecursive(
+    l1: ListNode | null,
+    l2: ListNode | null,
+    carry: number = 0
+): ListNode | null {
+    if (!l1 && !l2 && carry === 0) {
+        return null; // base case: nothing left to add
+    }
+
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+    const sum = val1 + val2 + carry;
+
+    const newNode = new ListNode(sum % 10);
+    newNode.next = addTwoNumbersRecursive(
+        l1 ? l1.next : null,
+        l2 ? l2.next : null,
+        Math.floor(sum / 10)
+    );
+
+    return newNode;
+}
