082e353a37f10c777cd421dcc865a4ac1087ef16
Trent <Trent.Jordan@ontariotechu.net>
2025-08-18 10:43:52 -0400

    TS twosum
    trying TS. I know JS and node so i figured its an easy transition

---
+++ 081825/twosum.ts
@@ -0,0 +1,13 @@
+function twoSum(nums: number[], target: number): number[] {
+    const seen: Map<number, number> = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (seen.has(complement)) {
+            return [seen.get(complement)!, i];
+        }
+        seen.set(nums[i], i);
+    }
+
+    return [];
+};
