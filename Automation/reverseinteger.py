1b8172f33fa03cd90b2d569cd663f42e918749f7
Trent <Trent.Jordan@ontariotechu.net>
2025-08-20 11:02:39 -0400

    reverse integer in one line
    with a little error handling

--- Automation/reverseinteger.py
+++ Automation/reverseinteger.py
@@ -18,3 +18,7 @@ class Solution:
             rev = rev * 10 + pop
 
         return sign * rev
+
+    def reverseOneLine(self, x: int) -> int:
+        s = int(str(abs(x))[::-1]) * (1 if x >= 0 else -1)
+        return s if -2**31 <= s <= 2**31 - 1 else 0
