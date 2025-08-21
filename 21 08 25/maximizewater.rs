4e93af20ba364a113db4c9434e70c029e43c75c4
Trent <Trent.Jordan@ontariotechu.net>
2025-08-21 12:24:40 -0400

    21/08/25 Container With Most Water
    Rust, O(n), 5 minutes

---
+++ 21 08 25/maximizewater.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = height.len() - 1;
+        let mut max_area = 0;
+
+        while left < right {
+            let h = std::cmp::min(height[left], height[right]);
+            let area = h * (right - left) as i32;
+            max_area = std::cmp::max(max_area, area);
+
+            if height[left] < height[right] {
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+
+        max_area
+    }
+}
