f434a5bc5d897295e4215f719c4b59a7fbfbb1f0
Trent <Trent.Jordan@ontariotechu.net>
2025-08-21 11:56:55 -0400

    Adding Stringless method
    O(logn) time

--- 21 08 25/palindromenumber.py
+++ 21 08 25/palindromenumber.py
@@ -6,3 +6,20 @@ class Solution:
         # Convert to string and check reverse
         s = str(x)
         return s == s[::-1]
+    
+    def isPalindromeStringless(self, x):
+        # Negative numbers are not palindromes
+        if x < 0:
+            return False
+        # Numbers ending with 0 (but not 0 itself) are not palindromes
+        if x % 10 == 0 and x != 0:
+            return False
+
+        rev = 0
+        while x > rev:
+            rev = rev * 10 + x % 10
+            x //= 10   # floor division for integer
+
+        # For even length: x == rev
+        # For odd length: x == rev // 10
+        return x == rev or x == rev // 10
