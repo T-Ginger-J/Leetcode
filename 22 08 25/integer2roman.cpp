a9dbc8822958e3f30f5d08d8d55aa9c42df9ab1c
Trent <Trent.Jordan@ontariotechu.net>
2025-08-22 11:41:45 -0400

    Create integer2roman.cpp

---
+++ 22 08 25/integer2roman.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> val = {1000, 900, 500, 400,
+                           100, 90, 50, 40,
+                           10, 9, 5, 4,
+                           1};
+        vector<string> syms = {"M", "CM", "D", "CD",
+                               "C", "XC", "L", "XL",
+                               "X", "IX", "V", "IV",
+                               "I"};
+        
+        string roman = "";
+        for (int i = 0; i < val.size(); ++i) {
+            while (num >= val[i]) {
+                num -= val[i];
+                roman += syms[i];
+            }
+        }
+        return roman;
+    }
+};
