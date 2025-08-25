169b7caf680e2d5219af59876d2abbf1df8374be
Trent <Trent.Jordan@ontariotechu.net>
2025-08-25 10:47:19 -0400

    Adding "one line" solution
    Slower, Less Readable, but cool looking

--- 25 08 25/roman2int.py
+++ 25 08 25/roman2int.py
@@ -5,6 +5,30 @@ VALUES = {
 
 class Solution:
     def romanToInt(self, s: str) -> int:
+        values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev = 0
+        
+        for c in reversed(s):
+            val = values[c]
+            if val < prev:
+                total -= val
+            else:
+                total += val
+            prev = val
+        
+        return total
+    
+    def romanToIntFast(self, s: str) -> int:
         total = 0
         prev = 0
         for i in range(len(s) - 1, -1, -1):
@@ -15,3 +39,7 @@ class Solution:
                 total += val
             prev = val
         return total
+
+    def romanToIntOneLine(self, s: str) -> int:
+        return sum((-1,1)[VALUES[s[i]] >= VALUES[s[i+1]]] * VALUES[s[i]]
+                   for i in range(len(s)-1)) + VALUES[s[-1]]
