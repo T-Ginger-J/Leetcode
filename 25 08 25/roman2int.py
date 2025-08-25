cfe749fa5e000d9aec8a3ac9983b91da661229a3
Trent <Trent.Jordan@ontariotechu.net>
2025-08-25 10:43:28 -0400

    Increasing Speed
    20% faster runtime by building global lookup table once

--- 25 08 25/roman2int.py
+++ 25 08 25/roman2int.py
@@ -1,24 +1,17 @@
+VALUES = {
+    'I': 1, 'V': 5, 'X': 10,
+    'L': 50, 'C': 100, 'D': 500, 'M': 1000
+}
+
 class Solution:
     def romanToInt(self, s: str) -> int:
-        values = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        
         total = 0
         prev = 0
-        
-        for c in reversed(s):
-            val = values[c]
+        for i in range(len(s) - 1, -1, -1):
+            val = VALUES[s[i]]
             if val < prev:
                 total -= val
             else:
                 total += val
             prev = val
-        
         return total
