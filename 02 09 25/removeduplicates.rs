6c2690ca3565953fe717596a02965ab03c156c67
Trent <Trent.Jordan@ontariotechu.net>
2025-09-02 12:53:41 -0400

    Rust Built in solution
    Experimenting with built in rust features

--- 02 09 25/removeduplicates.rs
+++ 02 09 25/removeduplicates.rs
@@ -16,6 +16,12 @@ impl Solution {
         k as i32
     }
 
+    pub fn remove_duplicates_built_in(nums: &mut Vec<i32>) -> i32 {
+        nums.dedup();  // removes consecutive duplicates in-place
+        nums.len() as i32
+    }
+}
+
 fn main() {
     let mut nums = vec![0,0,1,1,1,2,2,3,3,4];
     let k = Solution::remove_duplicates(&mut nums);
