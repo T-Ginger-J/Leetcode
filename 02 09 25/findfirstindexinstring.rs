2ca9d336303518e8285664fff20b1cd87bc8b36f
Trent <Trent.Jordan@ontariotechu.net>
2025-09-02 12:58:42 -0400

    02/08/25 Find the Index of the First Occurrence in a String
    Rust, O(n), 3 minutes

---
+++ 02 09 25/findfirstindexinstring.rs
@@ -0,0 +1,21 @@
+struct Solution;
+
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        let n = haystack.len();
+        let m = needle.len();
+        if m == 0 { return 0; }
+        if m > n { return -1; }
+
+        for i in 0..=n - m {
+            if &haystack[i..i + m] == needle {
+                return i as i32;
+            }
+        }
+        -1
+    }
+
+fn main() {
+    println!("{}", Solution::str_str("hello".to_string(), "ll".to_string())); // 2
+    println!("{}", Solution::str_str("aaaaa".to_string(), "bba".to_string())); // -1
+}
