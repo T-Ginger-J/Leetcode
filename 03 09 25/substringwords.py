b1f2aaaada117c83604f3e32b35802e57b1b3b4b
Trent <Trent.Jordan@ontariotechu.net>
2025-09-03 11:23:41 -0400

    Handling Edge Cases
    Test Case 182 on leetcode

--- 03 09 25/substringwords.py
+++ 03 09 25/substringwords.py
@@ -1,24 +1,34 @@
 from collections import Counter
 
 class Solution:
-    def findSubstring(self, s, words):
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
         if not s or not words:
             return []
 
         word_len = len(words[0])
-        total_len = word_len * len(words)
-        word_count = Counter(words)
+        word_count = len(words)
+        total_len = word_len * word_count
+        word_map = Counter(words)
         res = []
 
-        for i in range(len(s) - total_len + 1):
+        for i in range(word_len):  # start from every offset
+            left = i
             seen = Counter()
-            for j in range(0, total_len, word_len):
-                word = s[i + j:i + j + word_len]
-                if word not in word_count:
-                    break
-                seen[word] += 1
-                if seen[word] > word_count[word]:
-                    break
-            else:
-                res.append(i)
+            count = 0
+            for j in range(i, len(s) - word_len + 1, word_len):
+                word = s[j:j+word_len]
+                if word in word_map:
+                    seen[word] += 1
+                    count += 1
+                    while seen[word] > word_map[word]:
+                        left_word = s[left:left+word_len]
+                        seen[left_word] -= 1
+                        left += word_len
+                        count -= 1
+                    if count == word_count:
+                        res.append(left)
+                else:
+                    seen.clear()
+                    count = 0
+                    left = j + word_len
         return res
