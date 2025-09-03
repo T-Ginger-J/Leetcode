d1bff9f6e40bb4daee346c71899aeacd06e4f911
Trent <Trent.Jordan@ontariotechu.net>
2025-09-03 11:24:16 -0400

    Edge Cases Ruby
    Test Cases 179-182

--- 03 09 25/substringwords.rb
+++ 03 09 25/substringwords.rb
@@ -1,26 +1,40 @@
-class Solution
-  def find_substring(s, words)
-    return [] if s.empty? || words.empty?
+def find_substring(s, words)
+  return [] if s.empty? || words.empty?
 
-    word_len = words[0].length
-    total_len = word_len * words.length
-    word_count = Hash.new(0)
-    words.each { |w| word_count[w] += 1 }
-    result = []
+  word_len = words[0].size
+  word_count = words.size
+  total_len = word_len * word_count
+  word_map = Hash.new(0)
+  words.each { |w| word_map[w] += 1 }
+  res = []
 
-    (0..(s.length - total_len)).each do |i|
-      seen = Hash.new(0)
-      j = 0
-      while j < total_len
-        word = s[i + j, word_len]
-        break unless word_count.key?(word)
+  (0...word_len).each do |i| # start with each offset
+    left = i
+    seen = Hash.new(0)
+    count = 0
+
+    (i..s.size - word_len).step(word_len) do |j|
+      word = s[j, word_len]
+
+      if word_map.key?(word)
         seen[word] += 1
-        break if seen[word] > word_count[word]
-        j += word_len
-      end
-      result << i if j == total_len
-    end
+        count += 1
 
-    result
+        while seen[word] > word_map[word]
+          left_word = s[left, word_len]
+          seen[left_word] -= 1
+          left += word_len
+          count -= 1
+        end
+
+        res << left if count == word_count
+      else
+        seen.clear
+        count = 0
+        left = j + word_len
+      end
+    end
   end
+
+  res
 end
