7a8d59430746f89fb036a392d6fc98e7a6acb284
Trent <Trent.Jordan@ontariotechu.net>
2025-09-04 12:44:17 -0400

    04/09/25 Find First and Last Position of Element in Sorted Array
    PHP, O(logn), 6 minutes

---
+++ 04 09 25/firstlastelement.php
@@ -0,0 +1,37 @@
+<?php
+//O(logn)
+class Solution {
+    function searchRange($nums, $target) {
+        $first = $this->findBound($nums, $target, true);
+        if ($first == -1) {
+            return [-1, -1];
+        }
+        $last = $this->findBound($nums, $target, false);
+        return [$first, $last];
+    }
+
+    private function findBound($nums, $target, $isFirst) {
+        $left = 0;
+        $right = count($nums) - 1;
+        $bound = -1;
+
+        while ($left <= $right) {
+            $mid = intdiv($left + $right, 2);
+            if ($nums[$mid] == $target) {
+                $bound = $mid;
+                if ($isFirst) {
+                    $right = $mid - 1; // search left half
+                } else {
+                    $left = $mid + 1;  // search right half
+                }
+            } elseif ($nums[$mid] < $target) {
+                $left = $mid + 1;
+            } else {
+                $right = $mid - 1;
+            }
+        }
+
+        return $bound;
+    }
+}
+
