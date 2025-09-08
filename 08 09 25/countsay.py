b1845c389aad1cd6200cc173cdf7a1220954f0ed
Trent <Trent.Jordan@ontariotechu.net>
2025-09-08 13:30:54 -0400

    Count And Say Optimized

--- 08 09 25/countsay.py
+++ 08 09 25/countsay.py
@@ -18,3 +18,22 @@ class Solution:
 
     def countAndSayOneLine(self, n: int) -> str:
         return "1" if n == 1 else "".join(str(len(list(g))) + d for d, g in __import__("itertools").groupby(self.countAndSay(n - 1)))
+
+    def countAndSayOptimized(self, n: int) -> str:
+        s = "1"
+        for _ in range(n - 1):
+            res = []
+            prev = s[0]
+            count = 1
+            for c in s[1:]:
+                if c == prev:
+                    count += 1
+                else:
+                    res.append(str(count))
+                    res.append(prev)
+                    prev = c
+                    count = 1
+            res.append(str(count))
+            res.append(prev)
+            s = "".join(res)
+        return s
