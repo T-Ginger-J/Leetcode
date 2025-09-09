2c9080da7955de1705f606272fe4b4fe777ff942
Trent <Trent.Jordan@ontariotechu.net>
2025-09-09 12:33:09 -0400

    Combination Sum C#

---
+++ 09 09 25/combinationsum.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public IList<IList<int>> CombinationSum(int[] candidates, int target) {
+        var res = new List<IList<int>>();
+        Backtrack(candidates, target, 0, new List<int>(), res);
+        return res;
+    }
+
+    private void Backtrack(int[] candidates, int target, int start, List<int> path, IList<IList<int>> res) {
+        if (target == 0) {
+            res.Add(new List<int>(path));
+            return;
+        }
+
+        for (int i = start; i < candidates.Length; i++) {
+            if (candidates[i] <= target) {
+                path.Add(candidates[i]);
+                Backtrack(candidates, target - candidates[i], i, path, res); // can reuse same element
+                path.RemoveAt(path.Count - 1);
+            }
+        }
+    }
+}
+
