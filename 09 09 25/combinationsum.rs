5973fe43caae15e375eb2792a59ec8b6c4bb78bc
Trent <Trent.Jordan@ontariotechu.net>
2025-09-09 12:33:30 -0400

    Combination Sum Rust

---
+++ 09 09 25/combinationsum.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        fn backtrack(
+            candidates: &Vec<i32>,
+            target: i32,
+            start: usize,
+            path: &mut Vec<i32>,
+            res: &mut Vec<Vec<i32>>,
+        ) {
+            if target == 0 {
+                res.push(path.clone());
+                return;
+            }
+
+            for i in start..candidates.len() {
+                if candidates[i] <= target {
+                    path.push(candidates[i]);
+                    backtrack(candidates, target - candidates[i], i, path, res); // reuse same element
+                    path.pop();
+                }
+            }
+        }
+
+        let mut res = vec![];
+        let mut path = vec![];
+        backtrack(&candidates, target, 0, &mut path, &mut res);
+        res
+    }
+}
+
