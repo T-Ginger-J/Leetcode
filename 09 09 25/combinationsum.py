d5aba145d74c914d39337bf96eedae48f61957e8
Trent <Trent.Jordan@ontariotechu.net>
2025-09-09 12:31:23 -0400

    09/09/25 Combination Sum
    Python, O(n^2), 4 minutes

---
+++ 09 09 25/combinationsum.py
@@ -0,0 +1,23 @@
+#O(n^2)
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(start: int, path: List[int], total: int):
+            if total == target:
+                res.append(path[:])
+                return
+            if total > target:
+                return
+
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                # not i + 1 because we can reuse the same element
+                backtrack(i, path, total + candidates[i])
+                path.pop()
+
+        backtrack(0, [], 0)
+        return res
+
