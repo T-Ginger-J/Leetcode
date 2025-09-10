39b98933d560f8cc216918aaa19871427fe1d5f7
Trent <Trent.Jordan@ontariotechu.net>
2025-09-10 13:13:20 -0400

    Trap Water Main

--- 10 09 25/trapwater.dart
+++ 10 09 25/trapwater.dart
@@ -26,3 +26,9 @@ class Solution {
     return res;
   }
 
+
+void main() {
+  var sol = Solution();
+  print(sol.trap([0,1,0,2,1,0,1,3,2,1,2,1])); // 6
+  print(sol.trap([4,2,0,3,2,5]));             // 9
+}
