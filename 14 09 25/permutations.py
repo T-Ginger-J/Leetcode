b4a046496594f616ba0d28515f6248555f7e8910
Trent <Trent.Jordan@ontariotechu.net>
2025-09-14 15:09:12 -0400

    14/09/25 Permutations
    Python, O(n!), 3 minutes

---
+++ 14 09 25/permutations.py
@@ -0,0 +1,18 @@
+#O(n!)
+from typing import List
+from itertools import permutations
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def backtrack(path, remaining):
+            if not remaining:
+                res.append(path)
+                return
+            for i in range(len(remaining)):
+                backtrack(path + [remaining[i]], remaining[:i] + remaining[i+1:])
+        
+        backtrack([], nums)
+        return res
+    
