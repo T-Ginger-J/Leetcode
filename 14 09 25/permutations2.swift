3c7bb45c9641cf7c6baa1d991fcbeec072f72c27
Trent <Trent.Jordan@ontariotechu.net>
2025-09-14 15:11:19 -0400

    Permutations 2 Swift

---
+++ 14 09 25/permutations2.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func permuteUnique(_ nums: [Int]) -> [[Int]] {
+        var res = [[Int]]()
+        let nums = nums.sorted()
+        var used = [Bool](repeating: false, count: nums.count)
+        
+        func backtrack(_ path: [Int]) {
+            if path.count == nums.count {
+                res.append(path)
+                return
+            }
+            for i in 0..<nums.count {
+                if used[i] { continue }
+                if i > 0 && nums[i] == nums[i-1] && !used[i-1] { continue }
+                used[i] = true
+                var newPath = path
+                newPath.append(nums[i])
+                backtrack(newPath)
+                used[i] = false
+            }
+        }
+        
+        backtrack([])
+        return res
+    }
+}
+
