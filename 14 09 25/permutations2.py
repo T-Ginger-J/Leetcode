bf7aa88e45e19150c46b72aa047371facab2abc3
Trent <Trent.Jordan@ontariotechu.net>
2025-09-14 15:10:25 -0400

    14/09/25 Permutations II
    Python, O(n!), 4 minutes

---
+++ 14 09 25/permutations2.py
@@ -0,0 +1,26 @@
+#O(n!)
+from typing import List
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()  # sort to handle duplicates
+        
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+                    continue
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path, used)
+                path.pop()
+                used[i] = False
+        
+        backtrack([], [False]*len(nums))
+        return res
+
