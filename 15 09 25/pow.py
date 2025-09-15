150a98aa51905147adb115815e1de1e1b8b5bb45
Trent <Trent.Jordan@ontariotechu.net>
2025-09-15 14:27:11 -0400

    Recursive Solution

--- 15 09 25/pow.py
+++ 15 09 25/pow.py
@@ -11,3 +11,9 @@ class Solution:
             n >>= 1
         return res
 
+    def myPowRecursive(self, x: float, n: int) -> float:
+        if n == 0: return 1
+        if n < 0: return 1 / self.myPow(x, -n)
+        half = self.myPow(x, n // 2)
+        return half * half if n % 2 == 0 else half * half * x
+    
