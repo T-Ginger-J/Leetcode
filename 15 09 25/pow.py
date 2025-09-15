bceb2f6dbcbed789cb704e8d0beb4b2c9c5b21db
Trent <Trent.Jordan@ontariotechu.net>
2025-09-15 14:27:20 -0400

    One Line Solution

--- 15 09 25/pow.py
+++ 15 09 25/pow.py
@@ -17,3 +17,7 @@ class Solution:
         half = self.myPow(x, n // 2)
         return half * half if n % 2 == 0 else half * half * x
     
+    def myPowOneLine(self, x: float, n: int) -> float:
+        return 1 if n==0 else self.myPow(x*x, n//2)*(x if n%2 else 1) if n>0 else 1/self.myPow(x,-n)
+
+
