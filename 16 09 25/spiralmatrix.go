41d1b89d58af4cfe4216dc5b4ad65b388210fd9c
Trent <Trent.Jordan@ontariotechu.net>
2025-09-16 14:40:17 -0400

    Go Example Usage

--- 16 09 25/spiralmatrix.go
+++ 16 09 25/spiralmatrix.go
@@ -40,3 +40,10 @@ func spiralOrder(matrix [][]int) []int {
     return res
 }
 
+matrix := [][]int{
+    {1,2,3},
+    {4,5,6},
+    {7,8,9},
+}
+fmt.Println(spiralOrder(matrix))
+// Output: [1 2 3 6 9 8 7 4 5]
