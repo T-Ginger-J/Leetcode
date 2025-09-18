9d624f3867ca067cba7a65dd9889f391d07564b1
Trent <Trent.Jordan@ontariotechu.net>
2025-09-18 11:33:21 -0400

    Insert Interval Example Uses

--- 18 09 25/insertinterval.py
+++ 18 09 25/insertinterval.py
@@ -12,3 +12,7 @@ class Solution:
         res.append(newInterval)
         return res
     
+
+s = Solution()
+print(s.insert([[1,3],[6,9]], [2,5]))          # [[1,5],[6,9]]
+print(s.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))  # [[1,2],[3,10],[12,16]]
