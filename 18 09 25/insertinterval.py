b641ebba2b7e04b565cd38e7b43a3c3731fdba44
Trent <Trent.Jordan@ontariotechu.net>
2025-09-18 11:33:32 -0400

    One Line Solution

--- 18 09 25/insertinterval.py
+++ 18 09 25/insertinterval.py
@@ -12,6 +12,8 @@ class Solution:
         res.append(newInterval)
         return res
     
+    insertOneLine=lambda A,I:__import__('functools').reduce(lambda a,x:a+[x] if I[1]<x[0] and not a.append(I) else a if I[0]>x[1] or not a.append([min(I[0],x[0]),max(I[1],x[1])]) or not I.pop() else a,[x for x in A],[ ])+([I] if I else [])
+
 
 s = Solution()
 print(s.insert([[1,3],[6,9]], [2,5]))          # [[1,5],[6,9]]
