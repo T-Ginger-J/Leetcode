e4e615d8d5fc9d5873b07025f05d78d16fc36732
Trent <Trent.Jordan@ontariotechu.net>
2025-09-19 11:56:41 -0400

    Optimized Solution

--- 19 09 25/permutationsequence.py
+++ 19 09 25/permutationsequence.py
@@ -19,6 +19,20 @@ class Solution:
             result.append(nums.pop(index))
             k %= f
         return ''.join(result)
+    
+    def getPermutationOptimized(self, n: int, k: int) -> str:
+        fact = [1]*n
+        for i in range(1, n):
+            fact[i] = fact[i-1]*i
+        nums = [str(i) for i in range(1, n+1)]
+        k -= 1
+        res = []
+        for i in range(n-1, -1, -1):
+            idx = k // fact[i]
+            res.append(nums.pop(idx))
+            k %= fact[i]
+        return ''.join(res)
+    
 
 # Example usage:
 # sol = Solution()
