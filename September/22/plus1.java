2f473d24cb15e8e4cb6529b7d7a8159520ca50a0
Trent <Trent.Jordan@ontariotechu.net>
2025-09-22 12:01:04 -0400

    Plus 1 Java

---
+++ September/22/plus1.java
@@ -0,0 +1,23 @@
+// LeetCode 66: Plus One
+// Explanation:
+// 1. Traverse from last digit backwards.
+// 2. Handle carry if digit is 9.
+// 3. If all 9's, return array with extra digit.
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
+
