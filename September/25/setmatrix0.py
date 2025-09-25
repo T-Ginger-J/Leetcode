ead60633eb4939cba973dabcb8b89aa66d9415b7
Trent <Trent.Jordan@ontariotechu.net>
2025-09-25 12:04:28 -0400

    24/09/25 Set Matrix Zeroes
    Python, O(n^2), 3 minutes

---
+++ September/25/setmatrix0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def setZeroes(self, matrix: list[list[int]]) -> None:
+        rows, cols = set(), set()
+        m, n = len(matrix), len(matrix[0])
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in cols:
+                    matrix[i][j] = 05
