937f3919f67e8181e2dca01bdf1944efad3c0b50
Trent <Trent.Jordan@ontariotechu.net>
2025-09-25 12:07:56 -0400

    Search a 2D Matrix Optimized

--- September/25/search2d.py
+++ September/25/search2d.py
@@ -20,4 +20,32 @@ class Solution:
             else:
                 right = mid - 1
         return False
+    
+    def searchMatrixBinary(self, matrix: list[list[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        top, bot = 0, m - 1
+
+        while top <= bot:
+            row = (top + bot) // 2
+            if target < matrix[row][0]:
+                bot = row - 1
+            elif target > matrix[row][-1]:
+                top = row + 1
+            else:
+                break
+        else:
+            return False
+
+        row = (top + bot) // 2
+        l, r = 0, n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[row][mid] == target:
+                return True
+            elif matrix[row][mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
+    
     searchMatrixOneLine=lambda s,m,t:any(t in r for r in m)
