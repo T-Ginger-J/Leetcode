7e9b729826df5a4c757afa159c7a42f4d006a20c
Trent <Trent.Jordan@ontariotechu.net>
2025-09-26 00:10:47 -0400

    Optimized and One Line Solution

--- September/26/minwindowsubstring.py
+++ September/26/minwindowsubstring.py
@@ -7,6 +7,7 @@
 # Space Complexity: O(|s| + |t|)
 
 from collections import Counter
+from collections import defaultdict
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
@@ -34,6 +35,34 @@ class Solution:
         
         l, r = res
         return s[l:r+1] if res_len != float("inf") else ""
+
+    
+
+    def minWindow(self, s: str, t: str) -> str:
+        need = defaultdict(int)
+        for c in t:
+            need[c] += 1
+        missing = len(t)
+        l = start = end = 0
+        
+        for r, c in enumerate(s, 1):
+            if need[c] > 0:
+                missing -= 1
+            need[c] -= 1
+            
+            if missing == 0:
+                while l < r and need[s[l]] < 0:
+                    need[s[l]] += 1
+                    l += 1
+                if end == 0 or r - l < end - start:
+                    start, end = l, r
+                need[s[l]] += 1
+                missing += 1
+                l += 1
+        return s[start:end]
+    
+    class Solution: minWindow=lambda s,S:__import__('re').search('|'.join(sorted({S[i:j] for i in range(len(S)) for j in range(i+1,len(S)+1)})),s).group(0) if any(c in s for c in S) else ""
+
 
 # Example usage:
 # sol = Solution()
