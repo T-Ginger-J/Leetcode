7d552cceba09481f39a62c0e389338f766e4a74c
Trent <Trent.Jordan@ontariotechu.net>
2025-09-27 15:41:26 -0400

    Remove Duplicates from Sorted Array II  Examples

--- September/27/removedupesorted2.py
+++ September/27/removedupesorted2.py
@@ -15,3 +15,13 @@ class Solution:
         return k
 
     removeDuplicatesOneLine=lambda s,A:len([A.__setitem__(i,A[i])for i in range(len(A))if i<2 or A[i]!=A[i-2]])
+
+# Example usage:
+# sol = Solution()
+# nums = [1,1,1,2,2,3]
+# k = sol.removeDuplicates(nums)
+# print(k, nums[:k])  # 5 [1,1,2,2,3]
+#
+# nums = [0,0,1,1,1,1,2,3,3]
+# k = sol.removeDuplicates(nums)
+# print(k, nums[:k])  # 7 [0,0,1,1,2,3,3]
