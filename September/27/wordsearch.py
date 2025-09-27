b328cfe00d27b86d3d512693d8c46ae0a56ef458
Trent <Trent.Jordan@ontariotechu.net>
2025-09-27 15:38:52 -0400

    LeetCode 79: Word Search
    Python, O(n^2), 5 minutes

---
+++ September/27/wordsearch.py
@@ -0,0 +1,21 @@
+class Solution:
+    def exist(self, board: list[list[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c, idx):
+            if idx == len(word): return True
+            if r < 0 or r >= rows or c < 0 or c >= cols: return False
+            if board[r][c] != word[idx]: return False
+            
+            temp, board[r][c] = board[r][c], "#"
+            res = (dfs(r+1, c, idx+1) or
+                   dfs(r-1, c, idx+1) or
+                   dfs(r, c+1, idx+1) or
+                   dfs(r, c-1, idx+1))
+            board[r][c] = temp
+            return res
+        
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0): return True
+        return False
