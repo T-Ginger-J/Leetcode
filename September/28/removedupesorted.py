bbea91c82c60d99d6f6630dd98a9d8dd073c0645
Trent <Trent.Jordan@ontariotechu.net>
2025-09-28 01:12:58 -0400

    Remove Duplicates from Sorted List Examples

--- September/28/removedupesorted.py
+++ September/28/removedupesorted.py
@@ -21,3 +21,26 @@ class Solution:
         return head
 
     deleteDuplicatesOneLine=lambda s,h:h and (h.next:=s.deleteDuplicates(h.next)) or h
+
+
+# Example usage:
+# def list_to_linked(lst):
+#     dummy = ListNode(0)
+#     curr = dummy
+#     for v in lst:
+#         curr.next = ListNode(v)
+#         curr = curr.next
+#     return dummy.next
+#
+# def linked_to_list(node):
+#     res = []
+#     while node:
+#         res.append(node.val)
+#         node = node.next
+#     return res
+#
+# sol = Solution()
+# head = list_to_linked([1,1,2])
+# print(linked_to_list(sol.deleteDuplicates(head)))  # [1,2]
+# head = list_to_linked([1,1,2,3,3])
+# print(linked_to_list(sol.deleteDuplicates(head)))  # [1,2,3]
