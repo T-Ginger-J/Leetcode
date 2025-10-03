99bf55e598139a5ed07de797be4b5908cf4d0fd4
Trent <Trent.Jordan@ontariotechu.net>
2025-10-03 11:07:22 -0400

    Binary Tree Inorder Traversal Optimized

--- October/3/bintreeinordertravel.py
+++ October/3/bintreeinordertravel.py
@@ -18,3 +18,14 @@ class Solution:
         dfs(root)
         return res
 
+    def inorderTraversalStack(self, root: TreeNode):
+        res, stack = [], []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        return res
