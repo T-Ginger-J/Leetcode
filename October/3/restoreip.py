5962f00322cf56fdc894f0ed5906ae4d2d3c032a
Trent <Trent.Jordan@ontariotechu.net>
2025-10-03 11:05:10 -0400

    Restore IP Addresses Optimized

--- October/3/restoreip.py
+++ October/3/restoreip.py
@@ -34,3 +34,17 @@ class Solution:
         
         backtrack(0, [])
         return res
+
+    def restoreIpAddressesPruning(self, s: str) -> List[str]:
+        def valid(seg): 
+            return str(int(seg)) == seg and 0 <= int(seg) <= 255
+        res = []
+        n = len(s)
+        for i in range(1, min(4, n-2)):
+            for j in range(i+1, min(i+4, n-1)):
+                for k in range(j+1, min(j+4, n)):
+                    a, b, c, d = s[:i], s[i:j], s[j:k], s[k:]
+                    if all(map(valid, [a,b,c,d])):
+                        res.append(".".join([a,b,c,d]))
+        return res
+    
