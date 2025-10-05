30f4243150d75685b52f1838d301e08a1a682312
Trent <Trent.Jordan@ontariotechu.net>
2025-10-05 02:10:48 -0400

    LeetCode 101: Symmetric Tree
    Python, O(n), 2 minutes

---
+++ October/5/symtree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMirror(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2 or t1.val != t2.val:
+                return False
+            return isMirror(t1.left, t2.right) and isMirror(t1.right, t2.left)
+        return isMirror(root.left, root.right) if root else True
+
