9cd6d81a264f7d8cd4a8eaf0a22c1768870cd679
Trent <Trent.Jordan@ontariotechu.net>
2025-10-08 16:27:37 -0400

    eetCode 110: Balanced Binary Tree
    Python, O(n), 4 minutes

---
+++ October/8/balancedbintree.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return 0
+            left = dfs(node.left)
+            if left == -1:
+                return -1
+            right = dfs(node.right)
+            if right == -1 or abs(left - right) > 1:
+                return -1
+            return max(left, right) + 1
+
+        return dfs(root) != -1
