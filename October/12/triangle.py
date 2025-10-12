a6813e828e4123bf72d4a4f883de0f6d031901c0
Trent <Trent.Jordan@ontariotechu.net>
2025-10-12 01:51:03 -0400

    Triangle Optimized

--- October/12/triangle.py
+++ October/12/triangle.py
@@ -15,3 +15,10 @@ class Solution:
             for j in range(len(triangle[i])):
                 triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
         return triangle[0][0]
+
+    def minimumTotalDP(self, triangle):
+        dp = triangle[-1][:]
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+        return dp[0]
