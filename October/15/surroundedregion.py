032f66f73d30b67500af670e6252357c5b69d704
Trent <Trent.Jordan@ontariotechu.net>
2025-10-15 16:21:59 -0400

    Surrounded Regions Optimized

--- October/15/surroundedregion.py
+++ October/15/surroundedregion.py
@@ -40,3 +40,32 @@ class Solution:
                     board[r][c] = 'X'
                 elif board[r][c] == 'E':
                     board[r][c] = 'O'
+
+    def solvePruning(self, board: list[list[str]]) -> None:
+        if not board:
+            return
+        rows, cols = len(board), len(board[0])
+        queue = deque()
+        
+        for r in range(rows):
+            for c in [0, cols - 1]:
+                if board[r][c] == 'O':
+                    queue.append((r, c))
+        for c in range(cols):
+            for r in [0, rows - 1]:
+                if board[r][c] == 'O':
+                    queue.append((r, c))
+        
+        while queue:
+            r, c = queue.popleft()
+            if 0 <= r < rows and 0 <= c < cols and board[r][c] == 'O':
+                board[r][c] = 'E'
+                for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    queue.append((r+dr, c+dc))
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'E':
+                    board[r][c] = 'O'
