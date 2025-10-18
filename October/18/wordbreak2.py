169380f610b7899cc07408d341b1574bcee9484a
Trent <Trent.Jordan@ontariotechu.net>
2025-10-18 17:59:40 -0400

    Word Break II Examples

--- October/18/wordbreak2.py
+++ October/18/wordbreak2.py
@@ -30,3 +30,12 @@ class Solution:
             return res
 
         return dfs(0)
+
+print(Solution().wordBreak("catsanddog", ["cat","cats","and","sand","dog"]))
+# Output: ["cats and dog", "cat sand dog"]
+
+print(Solution().wordBreak("pineapplepenapple", ["apple","pen","applepen","pine","pineapple"]))
+# Output: ["pine apple pen apple","pineapple pen apple","pine applepen apple"]
+
+print(Solution().wordBreak("catsandog", ["cats","dog","sand","and","cat"]))
+# Output: []
