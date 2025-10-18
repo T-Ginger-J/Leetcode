c92f08aa60710f259d06d96e4f1e500a4536cc3c
Trent <Trent.Jordan@ontariotechu.net>
2025-10-18 17:57:00 -0400

    Single Number II Examples

--- October/18/singlenum2.py
+++ October/18/singlenum2.py
@@ -5,3 +5,12 @@ class Solution:
             ones = (ones ^ num) & ~twos
             twos = (twos ^ num) & ~ones
         return ones
+
+print(Solution().singleNumber([2,2,3,2]))
+# Output: 3
+
+print(Solution().singleNumber([0,1,0,1,0,1,99]))
+# Output: 99
+
+print(Solution().singleNumber([-2,-2,1,1,4,1,4,4,-4,-2]))
+# Output: -4
