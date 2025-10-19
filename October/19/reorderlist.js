83584e71e5bad51501724c5833353cb4256e9f0c
Trent <Trent.Jordan@ontariotechu.net>
2025-10-19 01:23:03 -0400

    Reorder List Examples

--- October/19/reorderlist.js
+++ October/19/reorderlist.js
@@ -43,3 +43,23 @@ var reorderList = function(head) {
         second = tmp2;
     }
 };
+
+function ListNode(val, next) {
+    this.val = val ?? 0;
+    this.next = next ?? null;
+}
+
+function printList(head) {
+    let res = [];
+    while (head) { res.push(head.val); head = head.next; }
+    console.log(res);
+}
+
+let head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+reorderList(head);
+printList(head); // ✅ [1, 4, 2, 3]
+
+let head2 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+reorderList(head2);
+printList(head2); // ✅ [1, 5, 2, 4, 3]
+
