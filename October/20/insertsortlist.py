5c4c1d1834404a20834d422faae926110faad3a3
Trent <Trent.Jordan@ontariotechu.net>
2025-10-20 19:20:59 -0400

    Insertion Sort List Optimized

--- October/20/insertsortlist.py
+++ October/20/insertsortlist.py
@@ -24,3 +24,22 @@ class Solution:
             prev.next = curr
             curr = nxt
         return dummy.next
+
+    def insertionSortListTail(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        dummy = ListNode(0, head)
+        last_sorted = head
+        curr = head.next
+        while curr:
+            if last_sorted.val <= curr.val:
+                last_sorted = last_sorted.next
+            else:
+                prev = dummy
+                while prev.next.val <= curr.val:
+                    prev = prev.next
+                last_sorted.next = curr.next
+                curr.next = prev.next
+                prev.next = curr
+            curr = last_sorted.next
+        return dummy.next
