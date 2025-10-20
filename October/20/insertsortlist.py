45206eee257ec2950049e95e31126565222faadc
Trent <Trent.Jordan@ontariotechu.net>
2025-10-20 19:20:37 -0400

    LeetCode 147: Insertion Sort List
    Python, O(n^2), 5 minutes

---
+++ October/20/insertsortlist.py
@@ -0,0 +1,13 @@
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        curr = head
+        while curr:
+            prev = dummy
+            while prev.next and prev.next.val < curr.val:
+                prev = prev.next
+            nxt = curr.next
+            curr.next = prev.next
+            prev.next = curr
+            curr = nxt
+        return dummy.next
