e6955605aeec4553b9ee418985a222bf427f13cf
Trent <Trent.Jordan@ontariotechu.net>
2025-10-26 22:44:17 -0400

    Majority Element Optimized

--- October/26/majorityelement.py
+++ October/26/majorityelement.py
@@ -14,3 +14,12 @@ class Solution:
             if counts[n] > len(nums) // 2:
                 return n
 
+    def majorityElementVoting(self, nums):
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        return candidate
+    
