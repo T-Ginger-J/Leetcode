7fc021986d3fcae27f2d95704e8c7ccfb1d5c764
Trent <Trent.Jordan@ontariotechu.net>
2025-10-27 22:35:01 -0400

    LeetCode 173: Binary Search Tree Iterator
    Python, O(1), 7 minutes

---
+++ October/27/binsearchtreeiterator.py
@@ -0,0 +1,19 @@
+class BSTIterator:
+    def __init__(self, root):
+        self.stack = []
+        self._push_left(root)
+    
+    def _push_left(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self):
+        node = self.stack.pop()
+        val = node.val
+        if node.right:
+            self._push_left(node.right)
+        return val
+
+    def hasNext(self):
+        return len(self.stack) > 0
