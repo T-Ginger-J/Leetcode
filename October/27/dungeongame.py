af4f6c991164d2bef94e73a5b09ab7aa5e97a811
Trent <Trent.Jordan@ontariotechu.net>
2025-10-27 22:36:29 -0400

    Dungeon Game Examples

--- October/27/dungeongame.py
+++ October/27/dungeongame.py
@@ -28,3 +28,11 @@ class Solution:
                 dp[j] = max(1, min(dp[j], dp[j + 1]) - dungeon[i][j])
         return dp[0]
 
+print(Solution().calculateMinimumHP([[-2,-3,3],[-5,-10,1],[10,30,-5]]))
+# Output: 7
+
+print(Solution().calculateMinimumHP([[0]]))
+# Output: 1
+
+print(Solution().calculateMinimumHP([[1,-2,3],[2,-2,-2]]))
+# Output: 2
