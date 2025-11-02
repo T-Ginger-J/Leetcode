2724a0fbda6680cf4b3320f03b64a6be43b04285
Trent <Trent.Jordan@ontariotechu.net>
2025-11-02 13:37:37 -0500

    Number of 1 Bits JS Examples

--- November/2/count1bit.js
+++ November/2/count1bit.js
@@ -15,3 +15,6 @@ var hammingWeight = function(n) {
     return count;
 };
 
+console.log(hammingWeight(0b00000000000000000000000000001011)); // 3
+console.log(hammingWeight(0b00000000000000000000000010000000)); // 1
+console.log(hammingWeight(0b11111111111111111111111111111101)); // 31
