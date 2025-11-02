f042246fd850f367201bbbbce536b66dfdbb1897
Trent <Trent.Jordan@ontariotechu.net>
2025-11-02 13:39:09 -0500

    Word Frequency Explanation

--- November/2/wordfreq.sh
+++ November/2/wordfreq.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# LeetCode 192: Word Frequency
+# Explanation:
+# Use Unix tools: tr, sort, uniq, awk to count word frequencies and sort descending.
+# Time Complexity: O(n log n) (due to sort)
+# Space Complexity: O(n)
+
 cat words.txt | tr -s ' ' '\n' | sort | uniq -c | sort -rn | awk '{ print $2, $1 }'
