e7b64e575c800e56f56f55b0d21f19ed47605c54
Trent <Trent.Jordan@ontariotechu.net>
2025-11-04 13:39:19 -0500

    Duplicate Emails Explanation

--- November/6/dupeemail.sql
+++ November/6/dupeemail.sql
@@ -1,3 +1,8 @@
+-- Explanation:
+-- We have a table `Person` with columns `id` and `email`.
+-- We need to find all emails that appear more than once.
+-- We group by email and use HAVING COUNT(email) > 1.
+
 SELECT 
     email
 FROM Person
