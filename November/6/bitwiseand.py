f176029558f222bd2eeaf452a6452df63b08a87f
Trent <Trent.Jordan@ontariotechu.net>
2025-11-05 14:53:51 -0500

    Bitwise AND of Numbers Range Examples

--- November/6/bitwiseand.py
+++ November/6/bitwiseand.py
@@ -22,3 +22,7 @@ class Solution:
 
     def rangeBitwiseAndOneLine(self, l, r): return r & (0 if l<r else l)
 
+sol = Solution()
+print(sol.rangeBitwiseAnd(5, 7))        # 4
+print(sol.rangeBitwiseAnd(0, 0))        # 0
+print(sol.rangeBitwiseAnd(1, 2147483647))  # 0
