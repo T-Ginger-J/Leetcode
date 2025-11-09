0a4722c2e7bb55a2173cc3cf0401df751c71f62f
Trent <Trent.Jordan@ontariotechu.net>
2025-11-08 21:56:46 -0500

    LeetCode 210: Course Schedule II
    Python, O(n), 7 minutes

---
+++ November/8/coursesched2.py
@@ -0,0 +1,22 @@
+from collections import *
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        order = []
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+            for nxt in graph[course]:
+                indegree[nxt] -= 1
+                if indegree[nxt] == 0:
+                    queue.append(nxt)
+
+        return order if len(order) == numCourses else []
