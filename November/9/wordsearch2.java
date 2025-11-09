314b10fc11582b245406e4626940cb7df14ad389
Trent <Trent.Jordan@ontariotechu.net>
2025-11-09 00:28:24 -0500

    Word Search II Java Examples

--- November/9/wordsearch2.java
+++ November/9/wordsearch2.java
@@ -52,3 +52,12 @@ class Solution {
     }
 }
 
+/* char[][] board = {
+    {'o','a','a','n'},
+    {'e','t','a','e'},
+    {'i','h','k','r'},
+    {'i','f','l','v'}
+};
+String[] words = {"oath","pea","eat","rain"};
+System.out.println(new Solution().findWords(board, words)); // ["oath", "eat"]
+*/
