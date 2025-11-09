9093e31c639e5e9b4edb23b9371ca3f41b2dd3b5
Trent <Trent.Jordan@ontariotechu.net>
2025-11-08 21:53:01 -0500

    LeetCode 208: Implement Trie (Prefix Tree)
    Python, O(n), 7 minutes

---
+++ November/8/implementtrie.py
@@ -0,0 +1,28 @@
+class Trie:
+
+    def __init__(self):
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        node = self.trie
+        for c in word:
+            if c not in node:
+                node[c] = {}
+            node = node[c]
+        node['#'] = True  # word end marker
+
+    def search(self, word: str) -> bool:
+        node = self.trie
+        for c in word:
+            if c not in node:
+                return False
+            node = node[c]
+        return '#' in node
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.trie
+        for c in prefix:
+            if c not in node:
+                return False
+            node = node[c]
+        return True
