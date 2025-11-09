364dc52395f09ac0001536ebdb8a513d01c7af29
Trent <Trent.Jordan@ontariotechu.net>
2025-11-08 21:56:19 -0500

    Minimum Size Subarray Sum JS Examples

--- November/8/minsizesubarrsum.js
+++ November/8/minsizesubarrsum.js
@@ -17,3 +17,7 @@ var minSubArrayLen = function(target, nums) {
     }
     return minLen === Infinity ? 0 : minLen;
 };
+
+console.log(minSubArrayLen(7, [2,3,1,2,4,3])); // 2
+console.log(minSubArrayLen(4, [1,4,4]));       // 1
+console.log(minSubArrayLen(11, [1,1,1,1,1]));  // 0
