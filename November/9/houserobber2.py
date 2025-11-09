71e05e23479d3051cad3f346fb51b7aee08a0f44
Trent <Trent.Jordan@ontariotechu.net>
2025-11-09 00:28:47 -0500

    LeetCode 213: House Robber II
    Python, O(n), 3 minutes

---
+++ November/9/houserobber2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        def rob_line(houses):
+            prev, curr = 0, 0
+            for n in houses:
+                prev, curr = curr, max(curr, prev + n)
+            return curr
+
+        return max(rob_line(nums[:-1]), rob_line(nums[1:]))
+
