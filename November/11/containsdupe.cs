ba02e40d005e792a60c6efd2330e4e9cc3ecd1ea
Trent <Trent.Jordan@ontariotechu.net>
2025-11-11 14:57:32 -0500

    Contains Duplicate Cs Examples

--- November/11/containsdupe.cs
+++ November/11/containsdupe.cs
@@ -20,3 +20,7 @@ public class Solution {
     }
 }
 
+// Solution sol = new Solution();
+// Console.WriteLine(sol.ContainsDuplicate(new int[]{1,2,3,1})); // True
+// Console.WriteLine(sol.ContainsDuplicate(new int[]{1,2,3,4})); // False
+// Console.WriteLine(sol.ContainsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2})); // True
