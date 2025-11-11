13e3c392fee4bd9bbcfd27c74fa81f1e1a680b4d
Trent <Trent.Jordan@ontariotechu.net>
2025-11-11 14:58:31 -0500

    The Skyline Problem Examples

--- November/11/skylineproblem.py
+++ November/11/skylineproblem.py
@@ -53,3 +53,10 @@ class Solution:
                 res.append([x, curr])
                 prev = curr
         return res
+
+sol = Solution()
+print(sol.getSkyline([[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]))
+# [[2,10],[3,15],[7,12],[12,0],[15,10],[20,8],[24,0]]
+
+print(sol.getSkyline([[0,2,3],[2,5,3]]))
+# [[0,3],[5,0]]
