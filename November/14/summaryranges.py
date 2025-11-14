36a6405fee924e4585955393aaaf430fbbf777f9
Trent <Trent.Jordan@ontariotechu.net>
2025-11-14 17:55:40 -0500

    LeetCode 228: Summary Ranges
    Python, O(n), 4 minutes

---
+++ November/14/summaryranges.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def summaryRanges(self, nums: list[int]) -> list[str]:
+        res = []
+        if not nums:
+            return res
+        
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1] + 1:
+                if start == nums[i - 1]:
+                    res.append(str(start))
+                else:
+                    res.append(f"{start}->{nums[i - 1]}")
+                start = nums[i]
+
+        # close last range
+        if start == nums[-1]:
+            res.append(str(start))
+        else:
+            res.append(f"{start}->{nums[-1]}")
+
+        return res
+
