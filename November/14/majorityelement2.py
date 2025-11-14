1ee62b8d30406db606d64c8d9d042c7813b6742e
Trent <Trent.Jordan@ontariotechu.net>
2025-11-14 17:56:47 -0500

    Majority Element II One Line

--- November/14/majorityelement2.py
+++ November/14/majorityelement2.py
@@ -5,3 +5,6 @@ class Solution:
         threshold = len(nums) // 3
         return [x for x, c in count.items() if c > threshold]
 
+    def majorityElementOneLine(self, nums: list[int]) -> list[int]:
+        return [x for x in set(nums) if nums.count(x) > len(nums)//3]
+
