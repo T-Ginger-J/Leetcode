26db1c41387cfefd7aee99b6ce1ba31cf6b138f3
Trent <Trent.Jordan@ontariotechu.net>
2025-11-15 14:52:22 -0500

    Kth Smallest Element in a BST Go Examples

--- November/15/kthsmallestinBST.go
+++ November/15/kthsmallestinBST.go
@@ -35,3 +35,11 @@ func kthSmallest(root *TreeNode, k int) int {
     }
 }
 
+// root := buildBST([]interface{}{3,1,4,nil,2})
+// fmt.Println(kthSmallest(root, 1))  // 1
+
+// root = buildBST([]interface{}{5,3,6,2,4,nil,1})
+// fmt.Println(kthSmallest(root, 3))  // 3
+
+// root = buildBST([]interface{}{2,1,3})
+// fmt.Println(kthSmallest(root, 2))  // 2
