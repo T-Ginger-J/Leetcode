15e4cc0d619d639b0b7e0057a608e89b48042cbd
Trent <Trent.Jordan@ontariotechu.net>
2025-11-17 20:54:28 -0500

    Delete Node in a Linked List Explanation

--- November/17/deletenodelinklist.py
+++ November/17/deletenodelinklist.py
@@ -1,3 +1,12 @@
+# LeetCode 237: Delete Node in a Linked List
+# Explanation:
+# You cannot access the head or previous node.
+# So instead:
+# - Copy the value of the next node into the current node.
+# - Skip the next node by pointing current.next to next.next
+#
+# Time Complexity: O(1)
+# Space Complexity: O(1)
 
 class Solution:
     def deleteNode(self, node):
