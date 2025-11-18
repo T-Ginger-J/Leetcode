3254c4d02291bf47d864eb47a556201bf11a68b5
Trent <Trent.Jordan@ontariotechu.net>
2025-11-17 20:55:28 -0500

    Product of Array Except Self Examples

--- November/17/productofarr.py
+++ November/17/productofarr.py
@@ -29,3 +29,8 @@ class Solution:
             suffix *= nums[i]
 
         return res
+
+# Example usage:
+# sol = Solution()
+# print(sol.productExceptSelf([1,2,3,4]))  # [24,12,8,6]
+# print(sol.productExceptSelf([-1,1,0,-3,3])) # [0,0,9,0,0]
