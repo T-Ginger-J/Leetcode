29931d7e5a258ea44534277cb87eac5f5f60f7de
Trent <Trent.Jordan@ontariotechu.net>
2025-11-18 23:06:15 -0500

    LeetCode 242: Valid Anagram
    Python, O(n), 3 minutes

---
+++ November/18/validanagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+        for ch in s:
+            count[ord(ch) - 97] += 1
+        for ch in t:
+            count[ord(ch) - 97] -= 1
+            if count[ord(ch) - 97] < 0:
+                return False
+
+        return True
