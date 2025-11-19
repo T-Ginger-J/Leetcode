af357b7359a42ece4ece6d011c7c13ba488188ed
Trent <Trent.Jordan@ontariotechu.net>
2025-11-18 23:04:27 -0500

    LeetCode 240: Search a 2D Matrix II
    Python, O(n), 4 minutes

---
+++ November/18/search2dmatrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        r, c = 0, len(matrix[0]) - 1
+
+        while r < len(matrix) and c >= 0:
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] > target:
+                c -= 1
+            else:
+                r += 1
+
+        return False
+
