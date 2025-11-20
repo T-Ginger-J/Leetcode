e36d8f96f068027064e34561d573d506d7e65c0a
Trent <Trent.Jordan@ontariotechu.net>
2025-11-20 03:49:33 -0500

    LeetCode 264: Ugly Number II
    Python, O(n), 7 minutes

---
+++ November/20/uglynum2.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly = [1] * n
+        i2 = i3 = i5 = 0
+        next2, next3, next5 = 2, 3, 5
+
+        for i in range(1, n):
+            next_ugly = min(next2, next3, next5)
+            ugly[i] = next_ugly
+
+            if next_ugly == next2:
+                i2 += 1
+                next2 = ugly[i2] * 2
+            if next_ugly == next3:
+                i3 += 1
+                next3 = ugly[i3] * 3
+            if next_ugly == next5:
+                i5 += 1
+                next5 = ugly[i5] * 5
+        return ugly[-1]
