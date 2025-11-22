03b968b19f5a085c7f617c334e7406cbc2d412b8
Trent <Trent.Jordan@ontariotechu.net>
2025-11-22 18:20:12 -0500

    Integer to English Words JS

---
+++ November/22/missingsolution.java
@@ -0,0 +1,14 @@
+// Explanation:
+// 1. Use the arithmetic sum formula 0..n and subtract array sum to get missing number.
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int num : nums) sum += num;
+        return n * (n + 1) / 2 - sum;
+    }
+}
+
