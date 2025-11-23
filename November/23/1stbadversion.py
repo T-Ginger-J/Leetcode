bb5277076dccf542966d19a347dc2871349e7cf7
Trent <Trent.Jordan@ontariotechu.net>
2025-11-23 17:07:35 -0500

    First Bad Version Examples

--- November/23/1stbadversion.py
+++ November/23/1stbadversion.py
@@ -8,3 +8,12 @@ class Solution:
             else:
                 left = mid + 1
         return left
+
+# Suppose version 4 is the first bad version
+bad_version = 4
+
+def isBadVersion(version):
+    return version >= bad_version
+
+print(Solution().firstBadVersion(5))  # Output: 4
+print(Solution().firstBadVersion(10)) # Output: 4
