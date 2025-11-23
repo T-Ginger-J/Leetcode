6cc3beab8424bbaf5f507cc2474799158aec2391
Trent <Trent.Jordan@ontariotechu.net>
2025-11-23 17:06:13 -0500

    H-Index II Cpp

---
+++ November/23/hindex2.cpp
@@ -0,0 +1,24 @@
+// Explanation:
+// 1. citations is sorted ascending.
+// 2. We binary search for minimum index i where citations[i] >= n - i.
+// 3. The h-index is n - i.
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (citations[m] >= n - m)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        return n - l;
+    }
+};
+
