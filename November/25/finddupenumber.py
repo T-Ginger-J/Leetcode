1c6193001cfce79efa7a844c59b722d1931edacd
Trent <Trent.Jordan@ontariotechu.net>
2025-11-25 18:01:19 -0500

    LeetCode 287: Find the Duplicate Number
    Python, O(n), 4 minutes

---
+++ November/25/finddupenumber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicate(self, nums):
+        # Phase 1: Detect intersection point
+        slow = fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        # Phase 2: Find entrance to cycle
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
