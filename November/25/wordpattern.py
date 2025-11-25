21cd5060516af61c0e561669bf55c3a0a0be4bda
Trent <Trent.Jordan@ontariotechu.net>
2025-11-25 18:04:25 -0500

    Word Pattern Optimized

--- November/25/wordpattern.py
+++ November/25/wordpattern.py
@@ -25,3 +25,10 @@ class Solution:
             p2w[p] = w
             w2p[w] = p
         return True
+
+    def wordPatternTuple(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        return len(set(zip(pattern, words))) == len(set(pattern)) == len(set(words))
+
