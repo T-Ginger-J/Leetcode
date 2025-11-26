7488df9450fb2480aff052ba0ac2d67ba6b5b98e
Trent <Trent.Jordan@ontariotechu.net>
2025-11-26 00:53:12 -0500

    Nim Game Examples

--- November/26/nimgame.py
+++ November/26/nimgame.py
@@ -9,3 +9,8 @@
 class Solution:
     def canWinNim(self, n: int) -> bool:
         return n % 4 != 0
+
+print(Solution().canWinNim(4))  # False
+print(Solution().canWinNim(1))  # True
+print(Solution().canWinNim(7))  # True
+print(Solution().canWinNim(8))  # False
