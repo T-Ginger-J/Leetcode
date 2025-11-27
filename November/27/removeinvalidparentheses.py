186444f604e81d0085a96ab78864a985717770c6
Trent <Trent.Jordan@ontariotechu.net>
2025-11-27 00:46:23 -0500

    Remove Invalid Parentheses Optimized

--- November/27/removeinvalidparentheses.py
+++ November/27/removeinvalidparentheses.py
@@ -48,3 +48,39 @@ class Solution:
             if found:
                 break
         return res
+
+    def removeInvalidParenthesesDFS(self, s: str):
+        res = set()
+        
+        def dfs(s, start, lremove, rremove):
+            if lremove == 0 and rremove == 0:
+                if self.is_valid(s):
+                    res.add(s)
+                return
+            for i in range(start, len(s)):
+                if i != start and s[i] == s[i-1]:
+                    continue
+                if lremove + rremove > len(s) - i:
+                    return
+                if lremove > 0 and s[i] == '(':
+                    dfs(s[:i]+s[i+1:], i, lremove-1, rremove)
+                if rremove > 0 and s[i] == ')':
+                    dfs(s[:i]+s[i+1:], i, lremove, rremove-1)
+        
+        lremove = rremove = 0
+        for c in s:
+            if c == '(': lremove += 1
+            elif c == ')':
+                if lremove > 0: lremove -= 1
+                else: rremove += 1
+        dfs(s, 0, lremove, rremove)
+        return list(res)
+    
+    def is_valid(self, s):
+        count = 0
+        for c in s:
+            if c == '(': count += 1
+            elif c == ')': count -= 1
+            if count < 0: return False
+        return count == 0
+    
