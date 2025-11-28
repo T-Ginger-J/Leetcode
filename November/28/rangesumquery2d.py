ade1f4db2fade04892f79fb86287daa1983030f1
Trent <Trent.Jordan@ontariotechu.net>
2025-11-28 10:16:53 -0500

    Range Sum Query 2D - Immutable Examples

--- November/28/rangesumquery2d.py
+++ November/28/rangesumquery2d.py
@@ -21,3 +21,14 @@ class NumMatrix:
     def sumRegion(self, row1, col1, row2, col2):
         return self.prefix[row2+1][col2+1] - self.prefix[row1][col2+1] - self.prefix[row2+1][col1] + self.prefix[row1][col1]
 
+matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+obj = NumMatrix(matrix)
+print(obj.sumRegion(2, 1, 4, 3))  # 8
+print(obj.sumRegion(1, 1, 2, 2))  # 11
+print(obj.sumRegion(1, 2, 2, 4))  # 12
