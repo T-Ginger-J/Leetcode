bcbe49181d7ac0977e27318ca983bd56c0931b63
Trent <Trent.Jordan@ontariotechu.net>
2025-12-02 19:12:47 -0500

    Coin Change Explanation

--- December/2/coinchange.py
+++ December/2/coinchange.py
@@ -1,3 +1,21 @@
+# LeetCode 322: Coin Change
+# Explanation:
+# We use classic bottom-up DP:
+#   dp[x] = minimum coins needed to make amount x
+#
+# Initialize:
+#   dp = [inf] * (amount + 1)
+#   dp[0] = 0  (0 coins needed to make 0)
+#
+# For each coin, update:
+#   dp[x] = min(dp[x], dp[x - coin] + 1)
+#
+# Result:
+#   dp[amount] if reachable else -1
+#
+# Time Complexity: O(n * amount)  (n = number of coins)
+# Space Complexity: O(amount)
+
 class Solution:
     def coinChange(self, coins, amount):
         INF = 10**9
