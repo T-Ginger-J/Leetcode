8d27ec7316c89fee101f9f3459ac0285080de44e
Trent <Trent.Jordan@ontariotechu.net>
2025-12-04 21:12:48 -0500

    LeetCode 329: Longest Increasing Path in a Matrix
    Python, O(n^2), 5 minutes

---
+++ December/4/longestincreasingpath.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestIncreasingPath(self, matrix):
+        if not matrix or not matrix[0]:
+            return 0
+        
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * n for _ in range(m)]  # memo table
+        
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        def dfs(i, j):
+            if dp[i][j] != 0:
+                return dp[i][j]
+            
+            best = 1  # Each cell is a path of length 1
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < m and 0 <= nj < n and matrix[ni][nj] > matrix[i][j]:
+                    best = max(best, 1 + dfs(ni, nj))
+            
+            dp[i][j] = best
+            return best
+        
+        longest = 1
+        for i in range(m):
+            for j in range(n):
+                longest = max(longest, dfs(i, j))
+        
+        return longest
