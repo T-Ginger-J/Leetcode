3bed1e6d7a1e38bad1ffe8121a18357d014e05e5
Trent <Trent.Jordan@ontariotechu.net>
2025-12-06 10:18:46 -0500

    LeetCode 337: House Robber III
    Python, O(n), 4 minutes

---
+++ December/6/houserobber3.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def rob(self, root):
+        def dfs(node):
+            if not node:
+                return (0, 0)  # (rob, not_rob)
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            rob = node.val + left[1] + right[1]
+            not_rob = max(left) + max(right)
+
+            return (rob, not_rob)
+
+        return max(dfs(root))
+
