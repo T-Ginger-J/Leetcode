0d184c7718addd412e0c8ac5fba6511ca6d5757b
Trent <Trent.Jordan@ontariotechu.net>
2025-12-06 10:16:21 -0500

    Flatten Nested List Iterator Optimized

--- December/7/flattennestedlistiter.py
+++ December/7/flattennestedlistiter.py
@@ -11,3 +11,25 @@ class NestedIterator:
             for x in reversed(lst):
                 self.stack.append(x)
         return bool(self.stack)
+
+class NestedIteratorInit:
+    def __init__(self, nestedList):
+        self.flat = []
+        self.index = 0
+
+        def dfs(lst):
+            for x in lst:
+                if x.isInteger():
+                    self.flat.append(x.getInteger())
+                else:
+                    dfs(x.getList())
+
+        dfs(nestedList)
+
+    def next(self):
+        val = self.flat[self.index]
+        self.index += 1
+        return val
+
+    def hasNext(self):
+        return self.index < len(self.flat)
