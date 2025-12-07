1642a000310eb031ea8d9eadaa01a74de065900e
Trent <Trent.Jordan@ontariotechu.net>
2025-12-07 00:17:45 -0500

    Power of Four Examples

--- December/7/power4.py
+++ December/7/power4.py
@@ -10,3 +10,7 @@
 class Solution:
     def isPowerOfFour(self, n):
         return n > 0 and (n & (n - 1)) == 0 and (n & 0x55555555) != 0
+
+print(Solution().isPowerOfFour(16))  # True
+print(Solution().isPowerOfFour(8))   # False
+print(Solution().isPowerOfFour(1))   # True
