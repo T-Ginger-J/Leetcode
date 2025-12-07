42d175eaca46fc5293d234fce480cd8292bf6f35
Trent <Trent.Jordan@ontariotechu.net>
2025-12-07 00:18:20 -0500

    Integer Break Optimized

--- December/7/intbreak.py
+++ December/7/intbreak.py
@@ -15,3 +15,12 @@ class Solution:
             res *= 3
             n -= 3
         return res * n
+
+    def integerBreakDP(self, n):
+        dp = [0] * (n + 1)
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], j * (i - j), j * dp[i - j])
+        return dp[n]
