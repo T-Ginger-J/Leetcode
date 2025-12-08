d4b5ab365f7800bf20fa2caa5339b6a11bd98e83
Trent <Trent.Jordan@ontariotechu.net>
2025-12-08 14:42:48 -0500

    Top K Frequent Elements (Bucket Sort) Optimized

--- December/8/topkfreqele.py
+++ December/8/topkfreqele.py
@@ -25,3 +25,16 @@ class Solution:
                 if len(result) == k:
                     return result
 
+    def topKFrequentMinHeap(self, nums, k):
+        from collections import Counter
+        import heapq
+
+        freq = Counter(nums)
+        heap = []
+
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for (count, num) in heap]
