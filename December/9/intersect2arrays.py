b4cd144771284551e26dac1f263ea9bf5b9c7c11
Trent <Trent.Jordan@ontariotechu.net>
2025-12-09 14:36:04 -0500

    Intersection of Two Arrays Examples

--- December/9/intersect2arrays.py
+++ December/9/intersect2arrays.py
@@ -8,3 +8,6 @@
 class Solution:
     def intersection(self, nums1, nums2):
         return list(set(nums1) & set(nums2))
+
+print(Solution().intersection([1,2,2,1], [2,2]))      # Output: [2]
+print(Solution().intersection([4,9,5], [9,4,9,8,4]))  # Output: [9,4] or [4,9]
