1a0fc4de30cd94bcdced90cbefe83978d33fe67e
Trent <Trent.Jordan@ontariotechu.net>
2025-12-14 21:47:28 -0500

    Kth Smallest Element in a Sorted Matrix Examples

--- December/14/kthsmallestele.py
+++ December/14/kthsmallestele.py
@@ -21,3 +21,11 @@ class Solution:
                 return val
             if c + 1 < n:
                 heapq.heappush(heap, (matrix[r][c+1], r, c+1))
+
+matrix = [
+    [1,5,9],
+    [10,11,13],
+    [12,13,15]
+]
+k = 8
+print(Solution().kthSmallest(matrix, k))  # Output: 13
