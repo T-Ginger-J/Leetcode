0b5398988c35f95a614668f9b199bbff959c0cbb
Trent <Trent.Jordan@ontariotechu.net>
2025-12-14 21:46:41 -0500

    Combination Sum IV Examples

--- December/14/combosum4.py
+++ December/14/combosum4.py
@@ -14,3 +14,8 @@ class Solution:
                 if i >= num:
                     dp[i] += dp[i - num]
         return dp[target]
+
+print(Solution().combinationSum4([1,2,3], 4))  # Output: 7
+# Combinations: [1,1,1,1],[1,1,2],[1,2,1],[2,1,1],[2,2],[1,3],[3,1]
+
+print(Solution().combinationSum4([9], 3))      # Output: 0
