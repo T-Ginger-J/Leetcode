4e59a74283e6117a21d2ca1c604313e7371024e1
Trent <Trent.Jordan@ontariotechu.net>
2025-12-14 21:45:44 -0500

    Wiggle Subsequence Examples

--- December/14/wigglesubseq.py
+++ December/14/wigglesubseq.py
@@ -16,3 +16,7 @@ class Solution:
             elif nums[i] < nums[i-1]:
                 down = up + 1
         return max(up, down)
+
+print(Solution().wiggleMaxLength([1,7,4,9,2,5]))  # Output: 6
+print(Solution().wiggleMaxLength([1,17,5,10,13,15,10,5,16,8]))  # Output: 7
+print(Solution().wiggleMaxLength([1,2,3,4,5,6,7,8,9]))  # Output: 2
