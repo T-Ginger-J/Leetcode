e8a7f988e2dc597447c27dee14b6cbdc37246992
Trent <Trent.Jordan@ontariotechu.net>
2025-12-15 14:29:27 -0500

    LeetCode 382: Linked List Random Node
    Python, O(n), 3 minutes

---
+++ December/15/linkedlistrandomnode.py
@@ -0,0 +1,20 @@
+import random
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def __init__(self, head: ListNode):
+        self.head = head
+
+    def getRandom(self) -> int:
+        result, node, i = 0, self.head, 1
+        while node:
+            if random.randint(1, i) == 1:
+                result = node.val
+            node = node.next
+            i += 1
+        return result
