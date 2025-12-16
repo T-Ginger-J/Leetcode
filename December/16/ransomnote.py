4b00a65414c0d6c983eebcd45c8d27d414033a96
Trent <Trent.Jordan@ontariotechu.net>
2025-12-16 11:58:10 -0500

    LeetCode 383: Ransom Note
    Python, O(n), 2 minutes

---
+++ December/16/ransomnote.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count = Counter(magazine)
+        for ch in ransomNote:
+            if count[ch] <= 0:
+                return False
+            count[ch] -= 1
+        return True
