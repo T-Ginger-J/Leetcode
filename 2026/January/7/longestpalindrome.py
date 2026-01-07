3f806864fa4a9c03bdc6a1e1c955a94587671f67
Trent <Trent.Jordan@ontariotechu.net>
2026-01-06 23:13:25 -0500

    LeetCode 409: Longest Palindrome
    Python, O(n), 3 minutes

---
+++ 2026/January/7/longestpalindrome.py
@@ -0,0 +1,15 @@
+
+from collections import Counter
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counts = Counter(s)
+        length = 0
+        odd_found = False
+        for c in counts.values():
+            if c % 2 == 0:
+                length += c
+            else:
+                length += c - 1
+                odd_found = True
+        return length + 1 if odd_found else length
