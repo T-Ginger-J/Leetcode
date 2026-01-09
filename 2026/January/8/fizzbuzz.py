e2080418d481af352bea58a49c697b69cf96d03a
Trent <Trent.Jordan@ontariotechu.net>
2026-01-09 14:01:05 -0500

    Fizz Buzz Example

--- 2026/January/8/fizzbuzz.py
+++ 2026/January/8/fizzbuzz.py
@@ -18,3 +18,8 @@ class Solution:
             else:
                 res.append(str(i))
         return res
+
+print(Solution().fizzBuzz(15))
+# Output:
+# ['1','2','Fizz','4','Buzz','Fizz','7','8','Fizz','Buzz',
+#  '11','Fizz','13','14','FizzBuzz']
