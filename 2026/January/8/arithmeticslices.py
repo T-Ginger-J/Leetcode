7717619aa503fd7145b57c201ea2fc146065d0a3
Trent <Trent.Jordan@ontariotechu.net>
2026-01-09 14:01:27 -0500

    LeetCode 413: Arithmetic Slices
    Python, O(n), 5 minutes

---
+++ 2026/January/8/arithmeticslices.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: list[int]) -> int:
+        total = 0
+        current = 0
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                current += 1
+                total += current
+            else:
+                current = 0
+        return total
