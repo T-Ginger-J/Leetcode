0ed3ff85f4e77752446985c6bafe2d27477ed5bc
Trent <Trent.Jordan@ontariotechu.net>
2026-01-11 10:55:29 -0500

    LeetCode 423: Reconstruct Original Digits from English
    Python, O(n), 5 minutes

---
+++ 2026/January/11/reconstructoriginaldigitsfromeng.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+class Solution:
+    def originalDigits(self, s: str) -> str:
+        count = Counter(s)
+        out = {}
+        
+        out['0'] = count['z']
+        out['2'] = count['w']
+        out['4'] = count['u']
+        out['6'] = count['x']
+        out['8'] = count['g']
+        
+        out['3'] = count['h'] - out['8']
+        out['5'] = count['f'] - out['4']
+        out['7'] = count['s'] - out['6']
+        out['1'] = count['o'] - out['0'] - out['2'] - out['4']
+        out['9'] = count['i'] - out['5'] - out['6'] - out['8']
+        
+        result = [k * out[k] for k in sorted(out.keys())]
+        return ''.join(result)
