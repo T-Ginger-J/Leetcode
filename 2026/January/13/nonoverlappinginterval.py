24b6241ac624053eff7fada9457a7c6571f0ef5c
Trent <Trent.Jordan@ontariotechu.net>
2026-01-13 13:57:50 -0500

    Non-overlapping Intervals Examples

--- 2026/January/13/nonoverlappinginterval.py
+++ 2026/January/13/nonoverlappinginterval.py
@@ -24,3 +24,18 @@ class Solution:
                 count += 1
         
         return count
+
+# Example 1
+intervals = [[1,2],[2,3],[3,4],[1,3]]
+# Output: 1 (remove [1,3])
+print(Solution().eraseOverlapIntervals(intervals))
+
+# Example 2
+intervals = [[1,2],[1,2],[1,2]]
+# Output: 2 (remove two overlapping intervals)
+print(Solution().eraseOverlapIntervals(intervals))
+
+# Example 3
+intervals = [[1,2],[2,3]]
+# Output: 0 (no overlaps)
+print(Solution().eraseOverlapIntervals(intervals))
