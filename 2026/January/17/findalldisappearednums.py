0ac6224b087783c119101009c0dcf3bca06e43bc
Trent <Trent.Jordan@ontariotechu.net>
2026-01-17 17:29:02 -0500

    LeetCode 448: Find All Numbers Disappeared in an Array
    Python, O(n), 2 minutes

---
+++ 2026/January/17/findalldisappearednums.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            index = abs(nums[i]) - 1
+            nums[index] = -abs(nums[index])
+        return [i + 1 for i, num in enumerate(nums) if num > 0]
