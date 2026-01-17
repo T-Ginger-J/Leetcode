8770b3781d3866122ded2fcdec4e577bbc3b160f
Trent <Trent.Jordan@ontariotechu.net>
2026-01-17 17:29:35 -0500

    Find All Numbers Disappeared in an Array Cpp Examples

--- 2026/January/17/findalldisappearednums.cpp
+++ 2026/January/17/findalldisappearednums.cpp
@@ -17,3 +17,30 @@ public:
         return res;
     }
 };
+
+int main() {
+    Solution sol;
+
+    // Example 1: Missing multiple numbers
+    vector<int> nums1 = {4,3,2,7,8,2,3,1};
+    vector<int> res1 = sol.findDisappearedNumbers(nums1);
+    for (int x : res1) cout << x << " ";
+    cout << endl;
+    // Expected output: 5 6
+
+    // Example 2: No missing numbers
+    vector<int> nums2 = {1,2,3,4,5};
+    vector<int> res2 = sol.findDisappearedNumbers(nums2);
+    for (int x : res2) cout << x << " ";
+    cout << endl;
+    // Expected output: (empty)
+
+    // Example 3: All numbers missing except one
+    vector<int> nums3 = {2,2,2,2};
+    vector<int> res3 = sol.findDisappearedNumbers(nums3);
+    for (int x : res3) cout << x << " ";
+    cout << endl;
+    // Expected output: 1 3 4
+
+    return 0;
+}
