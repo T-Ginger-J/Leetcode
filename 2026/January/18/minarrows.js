f9202a0f86673ca7805bfa03772465173620243c
Trent <Trent.Jordan@ontariotechu.net>
2026-01-18 13:54:52 -0500

    Minimum Number of Arrows to Burst Balloons JS Examples

--- 2026/January/18/minarrows.js
+++ 2026/January/18/minarrows.js
@@ -30,3 +30,16 @@ var findMinArrowShots = function(points) {
     return arrows;
 };
 
+// Additional Examples
+
+// Example 1: Overlapping intervals
+console.log(findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]));
+// Expected output: 2
+
+// Example 2: No overlaps
+console.log(findMinArrowShots([[1,2],[3,4],[5,6]]));
+// Expected output: 3
+
+// Example 3: All balloons overlap
+console.log(findMinArrowShots([[1,10],[2,9],[3,8],[4,7]]));
+// Expected output: 1
