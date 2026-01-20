576c87df2566d17738c51e3e2e7d02aaccc02754
Trent <Trent.Jordan@ontariotechu.net>
2026-01-19 20:56:16 -0500

    4Sum II Optimized

--- 2026/January/19/4sum2.py
+++ 2026/January/19/4sum2.py
@@ -37,3 +37,32 @@ class Solution:
                 count += ab_sum.get(-(c + d), 0)
 
         return count
+
+    # Method 2: Sorting + Two Pointers
+    def fourSumCountTwoPointers(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        AB = [a + b for a in A for b in B]
+        CD = [c + d for c in C for d in D]
+        AB.sort()
+        CD.sort()
+
+        i, j = 0, len(CD) - 1
+        count = 0
+
+        while i < len(AB) and j >= 0:
+            s = AB[i] + CD[j]
+            if s == 0:
+                a_val, c_val = AB[i], CD[j]
+                cnt_a = cnt_c = 0
+                while i < len(AB) and AB[i] == a_val:
+                    cnt_a += 1
+                    i += 1
+                while j >= 0 and CD[j] == c_val:
+                    cnt_c += 1
+                    j -= 1
+                count += cnt_a * cnt_c
+            elif s < 0:
+                i += 1
+            else:
+                j -= 1
+
+        return count
