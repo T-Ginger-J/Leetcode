0cdb57b2f77bde18a930724474b882adce5ecb56
Trent <Trent.Jordan@ontariotechu.net>
2026-01-20 12:19:47 -0500

    132 Pattern Explanation

--- 2026/January/20/132pattern.py
+++ 2026/January/20/132pattern.py
@@ -1,3 +1,17 @@
+# LeetCode 456: 132 Pattern
+# Explanation:
+# Given an array nums, determine if there exists a 132 pattern:
+# i < j < k such that nums[i] < nums[k] < nums[j].
+#
+# Method 1: Monotonic Stack (Optimal)
+# - Traverse from right to left.
+# - Maintain a decreasing stack for potential "3" values.
+# - Keep track of the best candidate for "2".
+# - If we find a value smaller than "2", a 132 pattern exists.
+#
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
 from typing import List
 
 class Solution:
