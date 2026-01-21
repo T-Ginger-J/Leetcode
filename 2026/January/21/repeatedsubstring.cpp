d2d232ded5f8ae3e9e8221deeccaf0415a33e09b
Trent <Trent.Jordan@ontariotechu.net>
2026-01-21 15:24:20 -0500

    Repeated Substring Pattern Cpp

---
+++ 2026/January/21/repeatedsubstring.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.size();
+        for (int len = 1; len <= n / 2; len++) {
+            if (n % len != 0) continue;
+            string sub = s.substr(0, len);
+            bool match = true;
+            for (int i = len; i < n; i += len) {
+                if (s.substr(i, len) != sub) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) return true;
+        }
+        return false;
+    }
+};
+
