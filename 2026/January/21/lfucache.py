952852cf78d30d711635aaf9ecadb81348faad08
Trent <Trent.Jordan@ontariotechu.net>
2026-01-21 15:25:03 -0500

    LeetCode 460: LFU Cache
    Python, O(1), 8 minutes

---
+++ 2026/January/21/lfucache.py
@@ -0,0 +1,38 @@
+from collections import defaultdict, OrderedDict
+
+class LFUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.key_to_val_freq = {}  # key -> (val, freq)
+        self.freq_to_keys = defaultdict(OrderedDict)  # freq -> keys
+        self.min_freq = 0
+
+    def _update_freq(self, key):
+        val, freq = self.key_to_val_freq[key]
+        del self.freq_to_keys[freq][key]
+        if not self.freq_to_keys[freq] and freq == self.min_freq:
+            self.min_freq += 1
+        self.key_to_val_freq[key] = (val, freq + 1)
+        self.freq_to_keys[freq + 1][key] = None
+
+    def get(self, key: int) -> int:
+        if key not in self.key_to_val_freq:
+            return -1
+        self._update_freq(key)
+        return self.key_to_val_freq[key][0]
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity == 0:
+            return
+        if key in self.key_to_val_freq:
+            self.key_to_val_freq[key] = (value, self.key_to_val_freq[key][1])
+            self._update_freq(key)
+            return
+        if len(self.key_to_val_freq) >= self.capacity:
+            # Evict least frequently used, oldest key
+            evict_key, _ = self.freq_to_keys[self.min_freq].popitem(last=False)
+            del self.key_to_val_freq[evict_key]
+        self.key_to_val_freq[key] = (value, 1)
+        self.freq_to_keys[1][key] = None
+        self.min_freq = 1
+
