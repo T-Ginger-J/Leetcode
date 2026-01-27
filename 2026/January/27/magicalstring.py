763b0bd67d5a6baef94e63c2068b148b3702a315
Trent <Trent.Jordan@ontariotechu.net>
2026-01-27 16:05:11 -0500

    LeetCode 481: Magical String
    Python, O(n), 3 minutes

---
+++ 2026/January/27/magicalstring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def magicalString(self, n: int) -> int:
+        if n <= 0:
+            return 0
+        if n <= 3:
+            return 1
+
+        s = [1, 2, 2]
+        i = 2
+        num = 1
+
+        while len(s) < n:
+            count = s[i]
+            s.extend([num] * count)
+            num = 3 - num
+            i += 1
+
+        return s[:n].count(1)
+
+
