e313d4d38b4825ad20e5f1453b2ea273e264b683
Trent <Trent.Jordan@ontariotechu.net>
2026-01-29 13:49:01 -0500

    Predict the Winner Optimized

--- 2026/January/29/predictthewinner.py
+++ 2026/January/29/predictthewinner.py
@@ -31,3 +31,23 @@ class Solution:
 
         return dp(0, n - 1) >= 0
 
+
+# Alternate Python Solution: Bottom-Up DP
+# - Use 2D DP table where dp[i][j] = max score difference for nums[i..j]
+# - Fill table for increasing length of subarrays
+
+class SolutionDP:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [[0]*n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = nums[i]
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1])
+
+        return dp[0][n-1] >= 0
+
+
