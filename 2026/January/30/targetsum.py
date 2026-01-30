c9d5c6c8092ff9d4c052440dd60f03cb2397a335
Trent <Trent.Jordan@ontariotechu.net>
2026-01-30 09:54:22 -0500

    LeetCode 494: Target Sum
    Python, O(n^2), 4 minutes

---
+++ 2026/January/30/targetsum.py
@@ -0,0 +1,13 @@
+from typing import List
+from functools import lru_cache
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        @lru_cache(None)
+        def dfs(i, total):
+            if i == len(nums):
+                return 1 if total == S else 0
+            return dfs(i+1, total + nums[i]) + dfs(i+1, total - nums[i])
+        
+        return dfs(0, 0)
+
