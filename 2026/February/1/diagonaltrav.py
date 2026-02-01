6591c0591c7a72da4102c480cb5fadb7f822f175
Trent <Trent.Jordan@ontariotechu.net>
2026-02-01 10:38:37 -0500

    Diagonal Traverse Optimized

--- 2026/February/1/diagonaltrav.py
+++ 2026/February/1/diagonaltrav.py
@@ -29,3 +29,40 @@ class Solution:
                 res.extend(intermediate)
         return res
 
+
+# Alternate Python Solution: Direct Simulation with Direction Toggle
+# - Keep track of current position and direction (up or down)
+# - Move diagonally and handle boundary cases
+
+class SolutionToggle:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        if not mat or not mat[0]:
+            return []
+        m, n = len(mat), len(mat[0])
+        res = []
+        i = j = 0
+        up = True
+        for _ in range(m * n):
+            res.append(mat[i][j])
+            if up:
+                if j == n - 1:
+                    i += 1
+                    up = False
+                elif i == 0:
+                    j += 1
+                    up = False
+                else:
+                    i -= 1
+                    j += 1
+            else:
+                if i == m - 1:
+                    j += 1
+                    up = True
+                elif j == 0:
+                    i += 1
+                    up = True
+                else:
+                    i += 1
+                    j -= 1
+        return res
+
