c2c0c11968c26b0296b7fe9e28212237f79ad51f
Trent <Trent.Jordan@ontariotechu.net>
2026-02-06 15:10:19 -0500

    Coin Change II Alternative

--- 2026/February/6/coinchange2.py
+++ 2026/February/6/coinchange2.py
@@ -39,3 +39,28 @@ class Solution:
                 dp[i] += dp[i - coin]
 
         return dp[amount]
+
+    # -------------------------------------------------------
+    # Method 2: 2D DP (Classic Knapsack)
+    # -------------------------------------------------------
+    def change2D(self, amount: int, coins: List[int]) -> int:
+
+        n = len(coins)
+
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
+
+        # Base case
+        for i in range(n + 1):
+            dp[i][0] = 1
+
+        for i in range(1, n + 1):
+            for j in range(amount + 1):
+
+                # Not use coin
+                dp[i][j] = dp[i - 1][j]
+
+                # Use coin (unlimited)
+                if j >= coins[i - 1]:
+                    dp[i][j] += dp[i][j - coins[i - 1]]
+
+        return dp[n][amount]
