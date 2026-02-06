0238988ef7f85fc8820ff13d92c50253808efabb
Trent <Trent.Jordan@ontariotechu.net>
2026-02-05 19:21:18 -0500

    Longest Palindromic Subsequence Alternate

--- 2026/February/5/longestpalindromesubseq.py
+++ 2026/February/5/longestpalindromesubseq.py
@@ -59,3 +59,27 @@ class Solution:
                     )
 
         return dp[0][n - 1]
+
+    # -------------------------------------------------------
+    # Method 2: Top-Down DP (Memoization)
+    # -------------------------------------------------------
+    def longestPalindromeSubseqMemo(self, s: str) -> int:
+
+        @lru_cache(None)
+        def dfs(l, r):
+
+            if l > r:
+                return 0
+            if l == r:
+                return 1
+
+            if s[l] == s[r]:
+                return dfs(l + 1, r - 1) + 2
+
+            return max(
+                dfs(l + 1, r),
+                dfs(l, r - 1)
+            )
+
+        return dfs(0, len(s) - 1)
+
