fc81e5e96050312e7cdf17658009b532c91c6a3c
Trent <Trent.Jordan@ontariotechu.net>
2026-02-11 08:05:35 -0500

    LeetCode 541: Reverse String II
    Python, O(n), 1 minute

---
+++ 2026/February/11/reversestring2.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    # -------------------------------------------------------
+    # Method 1: Slice Reversal
+    # -------------------------------------------------------
+    def reverseStr(self, s: str, k: int) -> str:
+
+        s = list(s)
+        n = len(s)
+
+        for i in range(0, n, 2 * k):
+            s[i:i+k] = reversed(s[i:i+k])
+
+        return ''.join(s)
+
