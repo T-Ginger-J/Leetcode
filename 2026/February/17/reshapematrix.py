9d24f1cee94c0b5a88f3e3e26346381fdbe3a76c
Trent <Trent.Jordan@ontariotechu.net>
2026-02-17 12:22:59 -0500

    Reshape the Matrix Examples

--- 2026/February/17/reshapematrix.py
+++ 2026/February/17/reshapematrix.py
@@ -34,3 +34,35 @@ class Solution:
             res[i//c][i%c] = mat[i//n][i%n]
         return res
 
+
+# -------------------------------------------------------
+# Examples & Edge Cases
+# -------------------------------------------------------
+
+sol = Solution()
+
+# Example 1
+mat1 = [[1,2],[3,4]]
+r1, c1 = 1,4
+print(sol.matrixReshape(mat1, r1, c1))           # [[1,2,3,4]]
+print(sol.matrixReshapeDirect(mat1, r1, c1))    # [[1,2,3,4]]
+
+# Example 2: same size, should return original
+mat2 = [[1,2],[3,4]]
+r2, c2 = 2,2
+print(sol.matrixReshape(mat2, r2, c2))           # [[1,2],[3,4]]
+
+# Example 3: impossible reshape
+mat3 = [[1,2],[3,4]]
+r3, c3 = 3,2
+print(sol.matrixReshape(mat3, r3, c3))           # [[1,2],[3,4]]
+
+# Example 4: 1x1 matrix
+mat4 = [[5]]
+r4, c4 = 1,1
+print(sol.matrixReshape(mat4, r4, c4))           # [[5]]
+
+# Example 5: larger reshape
+mat5 = [[1,2,3],[4,5,6]]
+r5, c5 = 3,2
+print(sol.matrixReshapeDirect(mat5, r5, c5))     # [[1,2],[3,4],[5,6]]
