89c457e0782e398ef4537ec554a310023ecced5c
Trent <Trent.Jordan@ontariotechu.net>
2026-02-17 12:22:52 -0500

    Reshape the Matrix Space Optimized

--- 2026/February/17/reshapematrix.py
+++ 2026/February/17/reshapematrix.py
@@ -22,3 +22,15 @@ class Solution:
         flat = [num for row in mat for num in row]
         return [flat[i*c:(i+1)*c] for i in range(r)]
 
+    # -------------------------------------------------------
+    # Method 2: Direct index mapping without extra list
+    # -------------------------------------------------------
+    def matrixReshapeDirect(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        if m*n != r*c:
+            return mat
+        res = [[0]*c for _ in range(r)]
+        for i in range(m*n):
+            res[i//c][i%c] = mat[i//n][i%n]
+        return res
+
