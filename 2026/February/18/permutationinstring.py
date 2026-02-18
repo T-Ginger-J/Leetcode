3aaa1a8cdb55d9a7b59010c22dda6adad51aecca
Trent <Trent.Jordan@ontariotechu.net>
2026-02-18 14:12:20 -0500

    Permutation in String Alternate

--- 2026/February/18/permutationinstring.py
+++ 2026/February/18/permutationinstring.py
@@ -31,3 +31,25 @@ class Solution:
                 return True
         return False
 
+    # -------------------------------------------------------
+    # Method 2: Sliding window with fixed-size array
+    # -------------------------------------------------------
+    def checkInclusionArray(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        from itertools import accumulate
+
+        freq1 = [0]*26
+        freq2 = [0]*26
+        for ch in s1:
+            freq1[ord(ch)-ord('a')] +=1
+        for ch in s2[:len(s1)]:
+            freq2[ord(ch)-ord('a')] +=1
+        if freq1 == freq2:
+            return True
+        for i in range(len(s1), len(s2)):
+            freq2[ord(s2[i])-ord('a')] +=1
+            freq2[ord(s2[i-len(s1)])-ord('a')] -=1
+            if freq1 == freq2:
+                return True
+        return False
