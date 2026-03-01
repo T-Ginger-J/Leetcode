1a19b64e72907399dc558b534eed20e0dddfc07a
Trent <Trent.Jordan@ontariotechu.net>
2026-02-28 23:14:01 -0500

    Merge Two Binary Trees Alternate

--- 2026/February/28/mergebintree.py
+++ 2026/February/28/mergebintree.py
@@ -90,3 +90,20 @@ class Solution:
         return root1
 
 
+    # Alternative Solution 2: Recursive In-Place Merge
+    def mergeTreesAlt2(self, root1: Optional[TreeNode],
+                       root2: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if not root1:
+            return root2
+
+        if not root2:
+            return root1
+
+        root1.val += root2.val
+
+        root1.left = self.mergeTreesAlt2(root1.left, root2.left)
+        root1.right = self.mergeTreesAlt2(root1.right, root2.right)
+
+        return root1
+
